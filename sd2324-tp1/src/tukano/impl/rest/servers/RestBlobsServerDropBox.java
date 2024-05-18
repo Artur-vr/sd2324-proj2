340bc9dc8b78a541ab7a9f737d14f00f8b85fb06
Artur-vr <artur.v.rego@gmail.com>
2024-05-18 12:20:05 +0100

    changes in dropbox server, passes till 105b

--- sd2324-tp1/src/tukano/impl/rest/servers/RestBlobsServerDropBox.java
+++ sd2324-tp1/src/tukano/impl/rest/servers/RestBlobsServerDropBox.java
@@ -32,8 +32,8 @@ public class RestBlobsServerDropBox extends AbstractRestServer {
 	public static void main(String[] args) {
 		Args.use(args);
 		new RestBlobsServerDropBox(Args.valueOf("-port", PORT)).start();
-		// if(Boolean.parseBoolean(args[0]) == true){
-		// 	DropBoxUtils.getInstance().cleanState();
-		// }
+		if(Boolean.parseBoolean(args[0]) == true){
+			DropBoxUtils.getInstance().cleanState();
+		}
 	}
 }
